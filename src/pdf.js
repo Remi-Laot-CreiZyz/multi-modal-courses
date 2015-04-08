259c295edd20269644e49c630e78c175ba2f2acf
remi <remilaot@gmail.com>
2015-04-08 04:58:32 +0200

    ...

--- src/pdf.js
+++ src/pdf.js
@@ -1,3 +1,163 @@
-$(document).ready(function(){
+var pdfUrl = null,
+	pdfDoc = null,
+	pdfPage = null,
+	scale = 1,
+	waitForLoadingPdfPage = false,
+	waitForLoadingPdfDocument = false,
+	canvas = null,
+	canvasContext = null;
 
+$(document).ready(function(){
+	//
+    // Disable workers to avoid yet another cross-origin issue (workers need the URL of
+    // the script to be loaded, and currently do not allow cross-origin scripts)
+    //
+    PDFJS.disableWorker = true;
+
+	canvas = document.getElementById('pdf-context');
+	canvasContext = canvas.getContext('2d');
+	var fragment = 'rapport_cles.pdf?page=1';
+	loadPage(fragment, canvas, canvasContext);
 });
+
+function loadPdf(fragment, callback, callbackParameters){
+	var url = fragment.split('?')[0];
+	console.log(url);
+	pdfDoc = null; // reset variable
+	pdfUrl = null;
+
+	if (!waitForLoadingPdfDocument){
+		waitForLoadingPdfDocument = true;
+		PDFJS.getDocument(url).then(function (pdfDoc_) {
+			waitForLoadingPdfDocument = false;
+			pdfUrl = url;
+			pdfDoc = pdfDoc_;
+			if (callback == loadPage) callback(callbackParameters[0], callbackParameters[1], callbackParameters[2]);
+		});
+	}
+}
+
+function loadPage(fragment, canvas, canvasContext){
+	if (!waitForLoadingPdfPage){
+		waitForLoadingPdfPage = true;
+		var parameters = fragment.split('?')[1].split('&'),
+			pageIndex = -1;
+
+		// Looks for page parameter
+		for (var i = 0; i < parameters.length; i++)
+			if (parameters[i].split('=')[0] == 'page')
+				pageIndex = i;
+
+		if (pageIndex != -1){
+			// We load the pdf
+		 	if (pdfUrl != fragment.split('?')[0]){
+		 		loadPdf(fragment, loadPage, [fragment, canvas, canvasContext]);
+				waitForLoadingPdfPage = false;
+		 	}
+		 	// We load the page
+		 	else {
+		 		pdfDoc.getPage(parseInt(parameters[pageIndex].split('=')[1])).then(function(page) {
+					var pdfViewBox = page.pageInfo.view;
+					var pdfPageWidth = pdfViewBox[2]*scale;
+					var pdfPageHeight = pdfViewBox[3]*scale;
+					var viewport = new PDFJS.PageViewport(pdfViewBox, scale, page.rotate, 0,0);
+
+					canvas.width = pdfPageWidth;
+					canvas.height = pdfPageHeight;
+					canvas.parentNode.width = pdfPageWidth;
+					canvas.parentNode.height = pdfPageHeight;
+
+					var renderTask = page.render({canvasContext:canvasContext,viewport:viewport});
+
+					// Wait for rendering to finish
+					renderTask.promise.then(function() {
+						waitForLoadingPdfPage = false;
+					});
+				});
+		 	}
+		}
+	}
+}
+
+function displayFragment(eventElement){
+	var fragment = eventElement.attr('data-fragment'),
+		parameters = fragment.split('?')[1].split('&'),
+		pageIndex = -1,
+		offsetXIndex = -1,
+		offsetYIndex = -1,
+		widthIndex = -1,
+		heightIndex = -1;
+
+	// Looks for page parameter
+	for (var i = 0; i < parameters.length; i++){
+		if (parameters[i].split('=')[0] == 'page')
+			pageIndex = i;
+		if (parameters[i].split('=')[0] == 'offsetx')
+			offsetXIndex = i;
+		if (parameters[i].split('=')[0] == 'offsety')
+			offsetYIndex = i;
+		if (parameters[i].split('=')[0] == 'width')
+			widthIndex = i;
+		if (parameters[i].split('=')[0] == 'height')
+			heightIndex = i;
+	}
+
+	console.log('avant: '+ pageIndex +" ; "+ offsetXIndex +" ; "+ offsetYIndex +" ; "+ widthIndex +" ; "+ heightIndex);
+	if (pageIndex != -1 && offsetXIndex != -1 && offsetYIndex != -1 && widthIndex != -1 && heightIndex != -1){
+	console.log('après');
+		$('#pdf-container').append('<div class="pdf-fragment"></div>');
+		var addedDiv = $('#pdf-container').children(':last');
+
+		addedDiv.width(parseFloat(parameters[widthIndex].split('=')[1]) * scale);
+		addedDiv.height(parseFloat(parameters[heightIndex].split('=')[1]) * scale);
+		addedDiv.css('left', ((parseFloat(parameters[offsetXIndex].split('=')[1]) * scale)));
+		addedDiv.css('top', ((parseFloat(parameters[offsetYIndex].split('=')[1]) * scale)));
+
+		// RESIZABLE FRAGMENT
+		addedDiv.resizable({
+			containment: "parent",
+			handles : "n,s,e,w,ne,nw,se,sw",
+			stop: function( event, ui ) {
+				var pdf_container = $('#pdf-container');
+				var newFragment = fragment.split('?')[0];
+				var width = Math.min((event.pageX - $(this).offset().left), pdf_container.width())/scale; // $(this).width n'est pas mit à jours lorsque l'event est lancé
+				var height = Math.min((event.pageY - $(this).offset().top), pdf_container.height())/scale; // $(this).height n'est pas mit à jours lorsque l'event est lancé
+				for (var i = 0; i < parameters.length; i++){
+					if (i != 0)
+						newFragment += '&';
+					if (parameters[i].split('=')[0] == 'offsetx')
+						newFragment += 'offsetx=' + ($(this).offset().left - pdf_container.offset().left)/scale;
+					else if (parameters[i].split('=')[0] == 'offsety')
+						newFragment += 'offsety=' + ($(this).offset().top - pdf_container.offset().top)/scale;
+					else if (parameters[i].split('=')[0] == 'width')
+						newFragment += 'width=' + width;
+					else if (parameters[i].split('=')[0] == 'height')
+						newFragment += 'height=' + height;
+					else
+						newFragment += parameters[i];
+				}
+				eventElement.attr('data-fragment', newFragment);
+			}
+		});
+
+		// DRAGGABLE FRAGMENT
+		addedDiv.draggable({
+			containment: "parent",
+			stop: function( event, ui ) {
+				var pdf_container = $('#pdf-container');
+				var newFragment = fragment.split('?')[0];
+				for (var i = 0; i < parameters.length; i++){
+					if (i != 0)
+						newFragment += '&';
+					if (parameters[i].split('=')[0] == 'offsetx')
+						newFragment += 'offsetx=' + ($(this).offset().left - pdf_container.offset().left)/scale;
+					else if (parameters[i].split('=')[0] == 'offsety')
+						newFragment += 'offsety=' + ($(this).offset().top - pdf_container.offset().top)/scale;
+					else
+						newFragment += parameters[i];
+				}
+				eventElement.attr('data-fragment', newFragment);
+			}
+		});
+	}
+}
--- src/timeline.js
+++ src/timeline.js
@@ -1,20 +1,78 @@
 $(document).ready(function(){
 	createTimeline($('#timeline'), 36.51);
+	
+	// $('#timeline').mouseover(function(e){
+	// 	$('#positionInTimeline').html('temps: ' + ((event.pageX/$(this).width())*parseFloat($(this).find(".timeline-panel").attr("data-duree"))).toFixed(2))
+	// });
+
+	$(window).resize(function(){
+		console.log("window resized");
+		// resizeTimeline($('#timeline'));
+	});
 });
 
 function createTimeline(timelineElement, duree){
+	timelineElement.addClass('row');
+	
 	/* control-panel-top */
-	timelineElement.append('<div class="control-panel-top"></div>');
-	var control_panel_top = timelineElement.find('.control-panel-top');
-
-	control_panel_top.append('<div class="video-controls"></div>');
-	control_panel_top.append('<div class="time-control"></div>');
+	timelineElement.append('<div class="control-panel col-md-2"></div>');
+	var control_panel = $('.control-panel');
+		// Video controls 
+		control_panel.append('<div class="video-controls"></div>');
+		var video_controls = control_panel.children('.video-controls');
+		video_controls.append('<div id="playpause" class="play"></div>');
+		video_controls.append('<div id="volume" class="volume-100"></div>');
+		video_controls.append('<div id="time" class="time-detail"></div>');
+			$('#time').append('<input type="text"/>');
+			$('#time').append('<span></span>');
+			$('#time span').html("/"+duree);
+		// Track controls
+		control_panel.append('<div class="track-controls"></div>');
 
 	/* timeline-panel */
-	timelineElement.append('<div class="timeline-panel" data-duree="'+duree+'"></div>');
+	timelineElement.append('<div class="timeline-panel col-md-10" data-duree="'+duree+'"></div>')
+	var timeline_panel = $('.timeline-panel');
+	// timeline_panel.resizable({
+	// 	handles: "n"
+	// });
+		// Time controls
+		timeline_panel.append('<div class="timeline-controls"></div>');
+		$('.timeline-controls').append('<div id="timeline-time-slider"></div>');
+		$('#timeline-time-slider').slider();
+		// Timeline tracks
+		timeline_panel.append('<div class="timeline-tracks-holder"></div>');
+		// Slider to choose
+		timeline_panel.append('<div class="timeline-zone-selector"></div>');
+		$('.timeline-zone-selector').append('<div id="timeline-zone-slider"></div>');
+		$('#timeline-zone-slider').slider({
+			range: true,
+			min: 0,
+			max: 100,
+			values: [0,100]
+		});
 
+
+
+	// TESTS
 	addTrack(timelineElement);
-	addEvent(getTrack(timelineElement, 0), {type:"pdf", startTime: 1, endTime: 2.5, fragment: "urlPdf/?offsetX=&offsetY=&width=&height="});
+	addTrack(timelineElement);
+	addTrack(timelineElement);
+	addTrack(timelineElement);
+	addTrack(timelineElement);
+	addTrack(timelineElement);
+	addTrack(timelineElement);
+	addTrack(timelineElement);
+	addTrack(timelineElement);
+	addTrack(timelineElement);
+	addTrack(timelineElement);
+	addTrack(timelineElement);
+	addTrack(timelineElement);
+	addTrack(timelineElement);
+	addEvent(getTrack(timelineElement, 0), {type:"pdf", startTime: 1, endTime: 2.5, fragment: "urlPdf/?page=2&offsetx=25&offsety=25&width=90&height=90"});
+	addEvent(getTrack(timelineElement, 2), {type:"pdf", startTime: 20, endTime: 30, fragment: "urlPdf/?offsetx=&offsety=&width=&height="});
+	addEvent(getTrack(timelineElement, 1), {type:"pdf", startTime: 15, endTime: 25.8, fragment: "urlPdf/?offsetx=&offsety=&width=&height="});
+	addEvent(getTrack(timelineElement, 0), {type:"pdf", startTime: 4, endTime: 13, fragment: "urlPdf/?offsetx=&offsety=&width=&height="});
+	displayFragment(getEvent(getTrack(timelineElement, 0), 0));
 	resizeTimeline(timelineElement);
 }
 
@@ -23,9 +81,8 @@ function resizeEvent(timeline, event){
 	var time_start = parseFloat(event.attr('data-start'));
 	var time_end = parseFloat(event.attr('data-end'));
 
-	console.log(duree_timeline + " ... " + time_start + " ... " + time_end);
-	
 	event.width(((time_end - time_start) / duree_timeline) * timeline.width());
+	event.css('left', (time_start/duree_timeline) * timeline.width() + timeline.offset().left);
 }
 
 function resizeTimeline(timeline){
@@ -36,39 +93,45 @@ function resizeTimeline(timeline){
 }
 
 function addTrack(timeline){
-	timeline.find('.timeline-panel').append('<div class="timeline-track"></div>');
+	timeline.find('.timeline-tracks-holder').append('<div class="timeline-track"></div>');
 }
 
 function addEvent(track, trackEvent){
 	track.append('<div class="timeline-track-event" data-type="'+trackEvent.type+'" data-start="'+trackEvent.startTime+'" data-end="'+trackEvent.endTime+'" data-fragment="'+trackEvent.fragment+'"></div>');
-	var eventDrag = track.children(":last");
-	eventDrag.draggable({
-		stop: function( event, ui ) {
-			
+	var addedEvent = track.children(":last");
 
-			var dureeEvent = ($(this).width()/track.width())*parseFloat(track.closest(".timeline-panel").attr("data-duree"));
-			var start = (($(this).offset().left-track.offset().left)/track.width())*parseFloat(track.closest(".timeline-panel").attr("data-duree"));
-			var end = start+dureeEvent;
-			$(this).attr("data-start", start);
-			$(this).attr("data-end", end);
-		},
+	addedEvent.draggable({
+		containment: "parent",
 		axis: "x",
-	});
-
-	eventDrag.resizable({
-		animate: true,
-		handles: "e",
-		stop: function(event, ui) {
-			var dureeEvent = ($(this).width()/track.width())*parseFloat(track.closest(".timeline-panel").attr("data-duree"));
+		stop: function( event, ui ) {
+			var dureeEvent = parseFloat($(this).attr('data-end'))-parseFloat($(this).attr('data-start'));
 			var start = (($(this).offset().left-track.offset().left)/track.width())*parseFloat(track.closest(".timeline-panel").attr("data-duree"));
 			var end = start+dureeEvent;
 			$(this).attr("data-start", start);
 			$(this).attr("data-end", end);
 		}
 	});
+
+	addedEvent.resizable({
+		containment: "parent",
+		animate: true,
+		handles: "e",
+		stop: function(event, ui) {
+			var width = event.pageX - $(this).offset().left; // $(this).width n'est pas mit à jours lorsque l'event est lancé
+			var dureeEvent = (width/track.width())*parseFloat(track.closest(".timeline-panel").attr("data-duree"));
+			var start = parseFloat($(this).attr("data-start"));
+			var end = Math.min(start + dureeEvent, parseFloat(track.closest(".timeline-panel").attr("data-duree")));
+			$(this).attr("data-end", end);
+		}
+	});
+
 }
 
 function getTrack(timeline, index)
 {
-	return timeline.find('.timeline-panel').children(':eq('+index+')');
+	return timeline.find('.timeline-tracks-holder').children(':eq('+index+')');
+}
+
+function getEvent(track, index){
+	return track.children(':eq('+index+')');
 }
